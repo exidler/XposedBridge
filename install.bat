6fde42c2bb27b9d277daed8f243006ff64d846a5
Oleg <xx.exis.git.oleg@kot.ms>
2013-11-01 15:50:08 +0400

    merge with revo/v36 done

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 cd /d %~dp0
 dir out\production\XposedBridge\XposedBridge.apk
 adb push out\production\XposedBridge\XposedBridge.apk /data/local/tmp
-adb shell su -c 'mv /data/local/tmp/XposedBridge.apk /data/xposed/XposedBridge.jar.newversion'
+adb shell su -c 'mv /data/local/tmp/XposedBridge.apk /data/data/de.robv.android.xposed.installer/XposedBridge.jar.newversion'
 pause
